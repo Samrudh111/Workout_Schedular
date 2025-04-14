b124df363b554521be850405ac4736a9c796290b
Samrudh111 <79571501+Samrudh111@users.noreply.github.com>
2025-04-13 19:19:18 -0500

    Update app.py

--- app.py
+++ app.py
@@ -20,6 +20,10 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db.init_app(app)
 
+# Run once to initialize the DB
+with app.app_context():
+    db.create_all()
+
 @app.route("/generate-plan", methods=["POST"])
 def generate_plan():
     # return jsonify([
@@ -82,12 +86,6 @@ def signup():
     db.session.commit()
 
     return jsonify({"message": "User created"}), 201
-
-# Run once to initialize the DB
-@app.before_first_request
-def create_tables():
-    db.create_all()
-
 
 if __name__ == "__main__":
     app.run(debug=True)
