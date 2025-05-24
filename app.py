d46eae547d61971ee8c570ffa57825580dc307e9
Samrudh111 <79571501+Samrudh111@users.noreply.github.com>
2025-05-24 11:59:26 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
-from models import db, User
+from models import db, User, WorkoutPlan
 import openai
 from openai import OpenAI
 import os
